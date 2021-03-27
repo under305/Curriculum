190d68aee9fd941d6d5a1dd77ec41f6fcf318d83
under305 <underjoel30@gmail.com>
2021-03-27 16:29:07 -0600

    Nuevos

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React, {useState} from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 
 import {
@@ -15,16 +16,38 @@ import {
 const App = () =>{
   const [contador, setContador] = useState(0);
 
+  const global = StyleSheet.create({
+    container: {
+      
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      height: 90,
+      padding: 35,
+    },
+    title: {
+      color: "black",
+      fontSize: 18,
+    },
+    btn: {
+      color: "black",
+      padding: "10%",
+      backgroundColor: "white",
+      borderRadius: "5px",
+      alignSelf: "center",
+      textAlign: "center",
+      margin: "5%",
+    },
+  });
+
   const aumentarContador = () =>{
     setContador(contador+1);
   }
 
   return(
-    <View>
+    <View style={global.container}>
       <Text>Texto tocado {contador} veces</Text>
-      <Text onPress={()=>{
-        aumentarContador();
-      }} >Hola mundo</Text>
+      <Text onPress={aumentarContador} >Hola mundo</Text>
     </View>
   );
 }
